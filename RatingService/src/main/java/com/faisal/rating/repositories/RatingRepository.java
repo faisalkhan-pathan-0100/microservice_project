91982e6d4332a7478fc88a5a24e9f89dc8ec15e0
Faisalkhan Pathan <faisalkhan.pathan.0100@gmail.com>
2025-11-27 15:38:21 +0530

    add custom finder method in RatingRepository to access the rating by users and by hotels

--- RatingService/src/main/java/com/faisal/rating/repositories/RatingRepository.java
+++ RatingService/src/main/java/com/faisal/rating/repositories/RatingRepository.java
@@ -4,6 +4,12 @@ import com.faisal.rating.entities.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RatingRepository extends JpaRepository<Rating,String> {
+
+    //custom finder methods
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
 }
